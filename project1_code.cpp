2f18a54a7f716b3d45902eb2fff3ac96d4580efa
Justin <sips96041016@gmail.com>
2018-10-12 00:06:36 +0800

    last_edition

--- project1_code.cpp
+++ project1_code.cpp
@@ -1,68 +1,58 @@
 #include <fstream>
 #include <string>
-
-#include <iostream>
-#include <time.h>
+#include <queue>
 
 using namespace std;
 int main(int argc,char* argv[]){
-
-    clock_t start, end;
-    double cpu_time_used;
-    start = clock();
-
-    int i,j,n,m,pos,posd,posu,value_d,*A;
-    int count = 0;
+    int i,j,n,m,pos,posd,tmp,count=0,value_d,*A;
     fstream fs;
     string str(argv[1]); 
+
+    queue<int> ind;
 
     fs.open(str+"/matrix.data", ios::in );
     if(!fs) return 1;
 
     fs>>m>>n;
 
-    A = new int[(m+2)*(n+2)];
-    for(j=0;j<n+2;j++) { A[j] = -2147483648; A[(m+1)*(n+2)+j] = -2147483648; }
-    A[(n+2)] = -2147483648;
-    for(j=1;j<=n;j++) fs>>A[(n+2)+j];
-    A[(n+2)+j] = -2147483648;
+    A = new int[2*(n+1)+1];
+    A[0] = -2147483648;
+    for(j=n+1;j<2*(n+1)+1;j++) A[j] = -2147483648;
+    for(j=1;j<=n;j++) fs>>A[j];
 
-    for(i=2;i<m+1;i++){
-        A[(n+2)*i] = -2147483648;
-        for(j=1;j<=n;j++){
-            pos = (n+2)*i+j;
-            posd = (n+2)*(i-1)+j;
-            posu = (n+2)*(i-2)+j;
+    for(i=1;i<m;i++){
+        for(j=0;j<n;j++){
+            pos = 1+(n+1)*(i%2)+j;
+            posd = 1+(n+1)*((i+1)%2)+j;
             value_d = A[posd];
-            fs>>A[pos];
-            if( value_d>=A[posu] && value_d>=A[posd-1] && value_d>=A[posd+1] && value_d>=A[pos] ) count++;
+            fs>>tmp;
+            if( value_d>=tmp && value_d>=A[posd-1] && value_d>=A[posd+1] && value_d>=A[pos] ){
+                ind.push(i-1); ind.push(j);
+                count++;
+            }
+            A[pos] = tmp;
         }
-        A[(n+2)*i+j] = -2147483648;
     }
     //last
-    A[(n+2)*i] = -2147483648;
-    for(j=1;j<=n;j++){
-        posd = (n+2)*m+j;
-        posu = (n+2)*(m-1)+j;
+    for(j=0;j<n;j++){
+        pos = 1+(n+1)*(i%2)+j;
+        posd = 1+(n+1)*((i+1)%2)+j;
         value_d = A[posd];
-        if( value_d>=A[posu] && value_d>=A[posd-1] && value_d>=A[posd+1] ) count++;
+        if( value_d>=A[pos] && value_d>=A[posd-1] && value_d>=A[posd+1] ) {
+            ind.push(i-1); ind.push(j);
+            count++;
+        }
     }
-    A[(n+2)*i+j] = -2147483648;
     fs.close();
-    fs.open(str+"final.peak", ios::out );
+    fs.open(str+"/final.peak", ios::out );
     fs<<count<<endl;
-    for(i=2;i<m+2;i++) for(j=1;j<n+1;j++){
-        pos = (n+2)*i+j;
-        posd = (n+2)*(i-1)+j;
-        posu = (n+2)*(i-2)+j;
-        value_d = A[posd];
-        if( value_d>=A[posu] && value_d>=A[posd-1] && value_d>=A[posd+1] && value_d>=A[pos] ) fs<<i-2<<" "<<j-1<<endl;
+    while(!ind.empty()) {
+        fs<<ind.front()<<" ";
+        ind.pop();
+        fs<<ind.front()<<endl;
+        ind.pop();
     }
     fs.close();
-
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    cout<<cpu_time_used<<endl;
 
     delete [] A;
     return 0;
